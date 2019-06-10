d094af09c1631d8578628101bd69a2691a57dbdf
Rupesh Ranjan Kumar <51476883+Raksas11@users.noreply.github.com>
2019-06-10 15:30:20 +0530

    rev.java

---
+++ rev.java
@@ -0,0 +1,26 @@
+import java.io.*;
+public class rev
+{
+ 	public static void main(String args[])
+	{
+		try
+		{
+	      DataInputStream in=new DataInputStream(System.in);
+	      int n;
+	      System.out.println("enter the number");
+	      n=Integer.parseInt(in.readLine());
+	      int rem,rev=0;
+	      while(n>0)
+		{
+		rem=n%10;
+		rev=rev*10+rem;
+		n=n/10;
+		}
+		System.out.println(rev);
+		}
+		catch(Exception e)
+		{
+		}
+	}
+}
+              
